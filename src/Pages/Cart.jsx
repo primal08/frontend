1ee1be9d23b2667eee8a58159265a663e502f0d3
primal <primal.miranda@visiongroupretail.com>
2025-07-27 20:59:00 +0530

    added media queries

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import CartItems from '../Components/CartItems/CartItems';
 
 const Cart = () => {
 
   return (
     <div>
-      
+      <CartItems/>
     </div>
   )
 }
